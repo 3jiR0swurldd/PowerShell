0a8abce7cb23ba81fe5df481ca093281f2921630
ejiroghene dan-Orogun <108234098+3jiR0swurldd@users.noreply.github.com>
2024-09-22 21:20:13 +0100

    Update NewuserScript.ps1

--- New User in Azure AD/NewuserScript.ps1
+++ New User in Azure AD/NewuserScript.ps1
@@ -1,2 +1,2 @@
-﻿$password = ConvertTo-SecureString -String "Password123" -AsPlainText -Force
+﻿$password = ConvertTo-SecureString -String "The intended Password" -AsPlainText -Force
 New-AzADUser -DisplayName "Killua Orogun" -Password $password -AccountEnabled $true -MailNickname killyorogun -UserPrincipalName KilluaOrogun@3jir0swurldd.onmicrosoft.com
